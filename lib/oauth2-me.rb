bb4d5bb7a3f76a845ffd3bf63c6f2d04f069bb6c
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2015-10-31 19:26:39 +0200

    wip

--- lib/oauth2-me.rb
+++ lib/oauth2-me.rb
@@ -3,31 +3,20 @@ require 'oauth2-me/omniauth_strategy_updater'
 require 'oauth2-me/railtie' if defined?(Rails)
 
 module OAuth2Me
-  class BannedEnvironmentError < StandardError; end
-  mattr_accessor :redirect_url
-  self.redirect_url= 'http://oauth2.me/redirect'
+  mattr_accessor :authorize_url
+  self.authorize_url= 'http://www.jetthoughts.com/oauth2me/request'
 
   class << self
-    def banned_environments
-      @@banned_environments = ['production']
-    end
-
     def omniauth_strategy_updater
       @@omniauth_strategy_updater ||= OAuth2Me::OmniauthStrategyUpdater.new
     end
 
     def setup_devise!
-      check_setup!
       Devise::OmniAuth::Config.send :define_method, :strategy_class_with_redirect do
         strategy_class = strategy_class_without_redirect
         OAuth2Me.omniauth_strategy_updater.setup_omniauth_strategy(strategy_class)
       end
       Devise::OmniAuth::Config.alias_method_chain :strategy_class, :redirect
-    end
-
-    def check_setup!
-      environment = ENV['RAILS_ENV'] || ENV['RACK_ENV']
-      raise BannedEnvironmentError.new if environment && banned_environments.include?(environment)
     end
   end
 end
--- lib/oauth2-me/omniauth_strategy_updater.rb
+++ lib/oauth2-me/omniauth_strategy_updater.rb
@@ -2,27 +2,26 @@ module OAuth2Me
   class OmniauthStrategyUpdater
     def setup_omniauth_strategy(strategy_class)
       if strategy_class < OmniAuth::Strategies::OAuth2
-        redefine_callback_url(strategy_class)
-        redefine_options_for(strategy_class)
+        redefine_client(strategy_class)
       end
       strategy_class
     end
 
     private
-    def redefine_callback_url(strategy_class)
-      strategy_class.send :define_method, :callback_url_with_redirect do
-        OAuth2Me.redirect_url
+    def redefine_client(strategy_class)
+      strategy_class.send :define_method, :client_with_redirect do
+        client_without_redirect.extend(ClientWithRedirectMethods)
       end
-      strategy_class.alias_method_chain :callback_url, :redirect
+      strategy_class.alias_method_chain :client, :redirect
     end
+  end
 
-    def redefine_options_for(strategy_class)
-      strategy_class.send :define_method, :options_for_with_redirect do |option|
-        return options_for_without_redirect(option) unless option == 'authorize'
-        encoded_callback_url = Base64.strict_encode64(callback_url_without_redirect)
-        options_for_without_redirect(option).merge(state: encoded_callback_url)
-      end
-      strategy_class.alias_method_chain :options_for, :redirect
+  module ClientWithRedirectMethods
+    def authorize_url(params = nil)
+      url = super
+      oauth2_me_uri = URI(OAuth2Me.authorize_url)
+      oauth2_me_uri.query = URI.encode_www_form(request_uri: url)
+      oauth2_me_uri.to_s
     end
   end
 end
